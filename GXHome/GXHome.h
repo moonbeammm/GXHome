5886ff98c0a38a350760aa974f6003c4209dc318
sunguangxin <sunguangxin@bilibili.com>
2017-08-10 17:24:37 +0800

    1.创建GXHome工程

---
+++ GXHome/GXHome.h
@@ -0,0 +1,19 @@
+//
+//  GXHome.h
+//  GXHome
+//
+//  Created by sunguangxin on 2017/8/10.
+//  Copyright © 2017年 sunguangxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GXHome.
+FOUNDATION_EXPORT double GXHomeVersionNumber;
+
+//! Project version string for GXHome.
+FOUNDATION_EXPORT const unsigned char GXHomeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GXHome/PublicHeader.h>
+
+
