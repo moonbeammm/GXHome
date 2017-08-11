763ab3ce13cf2680447bfb5dfc5ec41f15bc89f9
sunguangxin <sunguangxin@bilibili.com>
2017-08-11 19:42:00 +0800

    1.完成bus的搭建.并走通.

---
+++ GXHome/GXHome/Business/BusTest/GXHomeBusTestVC.h
@@ -0,0 +1,13 @@
+//
+//  GXHomeBusTestVC.h
+//  GXHome
+//
+//  Created by sunguangxin on 2017/8/11.
+//  Copyright © 2017年 sunguangxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GXHomeBusTestVC : UIViewController
+
+@end
---
+++ GXHome/GXHome/Business/BusTest/GXHomeBusTestVC.m
@@ -0,0 +1,22 @@
+//
+//  GXHomeBusTestVC.m
+//  GXHome
+//
+//  Created by sunguangxin on 2017/8/11.
+//  Copyright © 2017年 sunguangxin. All rights reserved.
+//
+
+#import "GXHomeBusTestVC.h"
+
+@interface GXHomeBusTestVC ()
+
+@end
+
+@implementation GXHomeBusTestVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor yellowColor];
+}
+
+@end
